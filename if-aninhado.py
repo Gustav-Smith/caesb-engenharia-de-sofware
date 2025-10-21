472aa0eb6fdb905d342a039869d2116afee88f48
Gustav-Smith <gustavj3an@gmail.com>
2025-10-21 00:03:33 -0300

    first commit

---
+++ if-aninhado.py
@@ -0,0 +1,10 @@
+chakra = 0
+jutsu = "Rasengan"
+
+if chakra > 50:
+    if jutsu == "Rasengan":
+        print("Naruto usa o Rasengan com força total!")
+    else:
+        print("Jutsu desconhecido, mas chakra suficiente.")
+else:
+    print("Chakra insuficiente para realizar jutsus.")
---
+++ python/if-elif-else.py
@@ -0,0 +1,10 @@
+personagem = "Naruto"
+
+if personagem == "Naruto":
+    print("Você escolheu o ninja da vila da folha!")
+elif personagem == "Sasuke":
+    print("Você escolheu o vingador do clã Uchiha!")
+elif personagem == "Sakura":
+    print("Você escolheu a medica ninja poderosa.")
+else:
+    print("Personagem desconhecido.")
